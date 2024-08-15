920d7ab0bb740d969b6449cba1d560b31d04d238
CristhianGT64 <crislopez_84@hotmail.es>
2024-08-15 03:26:04 -0600

    Se creo un nuevo impl y service para condcutores

--- src/main/java/hn/unah/bdi/uber/Services/CondcutoresService.java
+++ src/main/java/hn/unah/bdi/uber/Services/CondcutoresService.java
@@ -1,5 +1,7 @@
 package hn.unah.bdi.uber.Services;
 
+import org.springframework.stereotype.Service;
+
 public interface CondcutoresService {
     
 }
--- src/main/java/hn/unah/bdi/uber/Services/Impl/ConductoresImpl.java
+++ src/main/java/hn/unah/bdi/uber/Services/Impl/ConductoresImpl.java
@@ -2,9 +2,11 @@ package hn.unah.bdi.uber.Services.Impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
 
 import hn.unah.bdi.uber.Services.CondcutoresService;
 
+@Service
 public class ConductoresImpl implements CondcutoresService{
     
     @Autowired
